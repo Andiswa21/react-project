eda53d3a7ce561ef91797fcc4cd9f080d51b4eed
Andiswa Ngwenya <andiswa.ngwenya19@gmail.com>
2024-09-10 16:07:25 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+// import LandingPage from './LandingPage'
+import HomePage from './HomePage'
+
+function App() {
+
+  return (
+    <>
+    <HomePage />
+    </>
+  )
+}
+
+export default App
